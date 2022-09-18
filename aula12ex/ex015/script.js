35282c023bd4ecf01e215e1c37ca4796fb4e736f
Yago cayo <108165037+Yagocayo@users.noreply.github.com>
2022-09-17 22:35:57 -0300

    .

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -5,7 +5,7 @@ function verificar() {
     var res = document.querySelector('div#res') //duas formas de fazer a seleção
 
     if (fano.value.length == 0 || Number(fano.value) > ano) {
-        window.alert('[ERRO]... Verifique os dados e tente novamente!')
+        window.alert('[ERRO]  Verifique os dados e tente novamente!')
     } else {
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
