f0e8de5eeb286cc30ee2311968131624c52c588e
Yago cayo <108165037+Yagocayo@users.noreply.github.com>
2022-09-19 22:08:48 -0300

    trabalhando com while e do while

---
+++ aula13/ambiente.js
@@ -0,0 +1,17 @@
+
+
+var c = 1 //estrutura de repetição while
+do {
+    console.log(`Passo ${c}`)
+    c = c + 1
+} while (c <= 10) 
+
+
+
+/*
+var c = 1 //estrutura de repetição while
+while (c <= 10){
+    console.log(`Passo ${c}`)
+    c = c + 1
+}
+*/
