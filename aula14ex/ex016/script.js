e8877f7172a35524944a0e8f4d9b0595b2d5fa29
Yago cayo <108165037+Yagocayo@users.noreply.github.com>
2022-09-20 22:34:31 -0300

    entendendo estruturas de reptição do codigo javascript

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,3 +1,38 @@
-function contando() {
-    
+function contar () {
+    let ini = window.document.getElementById('txti')
+    let fim = window.document.getElementById('txtf')
+    let passo = window.document.getElementById('txtp')
+    let res = window.document.getElementById('res')
+
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        res.innerHTML = 'Impossivel contar!'
+        //window.alert('[ERRO] Faltam dados!')
+    } else {
+       res.innerHTML = 'contando: <br> '
+       let i = Number(ini.value)
+       let f = Number(fim.value)
+       let p = Number(passo.value)
+       if (p <= 0) {
+        window.alert('passo inválido! Considerando PASSO 1')
+        p = 1
+       }
+
+       if (i < f)
+       // contagem crescente
+            for (let c = i; c <= f; c += p){
+                    res.innerHTML += `${c} \u{1f449}`//  codigo do emoji so funciona entre crases `\u{1f449}`
+            }
+            
+        else { 
+            // contagem regressiva
+            for(let c = i; c>= f; c -= p ) {
+                res.innerHTML += `${c} \u{1f449}`
+
+            }
+            
+        }
+        res.innerHTML += `\u{1f3c1}`
+
+    }
+
 }
