412a083ef0a62a29892080799ce423cfdf280036
Yago cayo <108165037+Yagocayo@users.noreply.github.com>
2022-09-26 11:46:44 -0300

    .

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -1,11 +1,11 @@
 let num = [5, 8, 2, 9, 3]
 
-num.push(1)
-num.sort()
+num.push(1) //adiciona uma variavel no final 
+num.sort() //organiza as variaveis de forma crescente
 console.log(num)
 console.log(`O vetor tem  ${num.length} posições`)
 console.log(`o primeiro valor do vetor é ${num[0]}`)
-let pos = num.indexOf(8)
+let pos = num.indexOf(8)// identifica a posição de uma variavel
 if (pos == -1){
     console.log('o valor nao foi encontrado!')
 }
